4cec624c19d995c0e6034fef349b125aaeb14f3d
RYUL99 <seunglul1999@gmail.com>
2023-10-26 17:36:38 +0900

    ê기본 환경설정 후 Controller, Service ë§í기본 틀만 만들어줌

---
+++ src/main/java/com/faceof/prototype/controller/FaceOfController.java
@@ -0,0 +1,9 @@
+package com.faceof.prototype.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+// @Controller 안에 스프링 @Component가 있어서 스프링 빈 자동 등록
+public class FaceOfController {
+}
---
+++ src/main/java/com/faceof/prototype/service/FaceOfService.java
@@ -0,0 +1,9 @@
+package com.faceof.prototype.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+// @Service 안에 스프링 @Component가 있어서 스프링 빈 자동 등록
+public class FaceOfService {
+}
